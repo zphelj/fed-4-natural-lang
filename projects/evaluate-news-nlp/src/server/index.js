290f6bb4197eac43fcc924da8aa54bf211754351
Jay Phelps <jay@jayphelps.name>
2020-09-12 14:18:36 -0500

    Missed a file in last commit!

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your MeaningCloud API key is ${process.env.MEANINGCLOUD_API_KEY}`);
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
