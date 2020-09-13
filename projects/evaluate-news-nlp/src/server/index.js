c34641543253ab3abc174cd29e0afe1c655c84af
Jay Phelps <jay@jayphelps.name>
2020-09-12 20:13:58 -0500

    Fixed dotenv load and handle exception.

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,27 +1,53 @@
+// we have several variables coming from the environment
 const dotenv = require('dotenv');
-dotenv.config();
-console.log(`Your MeaningCloud API key is ${process.env.MEANINGCLOUD_API_KEY}`);
+const result = dotenv.config()
+if (result.error) {
+  throw result.error
+}
+// console.log(result.parsed)
+// ENV loaded properly
+const meaningCloudAPIkey = process.env.MEANINGCLOUD_API_KEY;
+const serverport = process.env.NLP_SERVER_PORT;
 
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser');
+// const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
 app.use(express.static('dist'))
 
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+res.sendFile('dist/index.html')
+// res.sendFile(path.resolve('src/client/views/index.html'))
 })
+
+app.post('/process_nlp_url', function (req, res) {
+  console.log('POST: NLP requested:');
+  console.log("URL = ", req.body.url);
+  res.send(projectData);
+});
+
+// debug testing
+app.get('/hello', (req, res) => {
+  console.log('GET: Hello was requested');
+  res.send("Hello World!");
+});
+
+/* app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+}) */
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.listen(serverport, function () {
+  console.log(`Natural Language Server app listening on port ${serverport}!`);
+  console.log(`Your MeaningCloud API key is ${meaningCloudAPIkey}`);
+});
